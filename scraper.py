799457705041c2dd35ac5d1d5e3d935a677994b1
bulongo mukkuli <bmukkuli@gmail.com>
2020-07-30 14:59:53 +0200

    Added exception handling

--- scraper.py
+++ scraper.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup as bs
+from requests.exceptions import MissingSchema
 import requests
 import lxml
 
 site = input('Name of the site: ')
-holder = requests.get(site)
+try:
+    holder = requests.get(site)
+except MissingSchema:
+    holder = requests.get('http://' + site)
+except MissingSchema:
+    holder = requests.get('https://' + site)
 soup = bs(holder.text,'lxml')
 
 item = input('What type of file would you like to find?: ("a-for links and so on" ')
